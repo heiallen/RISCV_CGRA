b0d42da944efb237dffe7614bcd9b70ec81bab78
heiallen <wangchongyang96@163.com>
2021-06-19 00:19:18 +0800

    commit

--- core/ifu_ifetch.v
+++ core/ifu_ifetch.v
@@ -87,6 +87,97 @@ sirv_gnrl_dfflr #(1) reset_req_dfflr(reset_req_ena,reset_req_nxt,reset_req_r,clk
 wire ifu_reset_req = reset_req_r;
 
 
+//////////////////////////////////////////////////////////////////
+//the halt ack generation
+
+wire halt_ack_set;
+wire halt_ack_clr;
+wire halt_ack_ena;
+wire halt_ack_r;
+wire halt_ack_nxt;
+
+//the halt_ack will be set when
+//  currently halt_req is asserting
+//  currently halt_ack is not asserting
+//  currently the ifetch REQ channel is ready, means there is no outstanding transactions
+
+wire ifu_no_outs;
+assign halt_ack_set = ifu_halt_req & (~halt_ack_r) & ifu_no_outs;
+
+//the halt_ack_r valid is cleared when 
+//  currently halt_ack is asserting
+//  currently halt_req is de-asserting
+assign halt_ack_clr = halt_ack_r & (~ifu_halt_req);
+assign halt_ack_ena = halt_ack_set | halt_ack_clr;
+assign halt_ack_nxt = halt_ack_set | (~halt_ack_clr);
+
+sirv_gnrl_dfflr #(1) halt_ack_dfflr (halt_ack_ena,halt_ack_nxt,halt_ack_r,clk,rst_n);
+
+assign ifu_halt_ack = halt_ack_r;
+
+
+/////////////////////////////////////////////////////////////////////
+//the flush ack signal generation
+
+assign pipe_flush_ack = 1'b1;
+
+wire dly_flush_set;
+wire dly_flush_clr;
+wire dly_flush_ena;
+wire dly_flush_nxt;
+
+//the delay flush will be set when there is a flush request is coming, but
+//the ifu is not ready to accept new fetch request
+wire dly_flus_r;
+assign dly_flush_set = pipe_flush_req & (~ifu_req_hsked);
+
+//the dly_flush_r valid is cleared when the delayde flush is issued
+assign dly_flush_clr = dly_flush_r & ifu_req_hsked;
+assign dly_flush_ena = dly_flush_set | dly_flush_clr;
+assign dly_flush_nxt = dly_flush_set | (~dly_flush_clr);
+
+sirv_gnrl_dfflr #(1) dly_flush_dfflr (dly_flush_ena, dly_flush_nxt, dly_flush_r, clk, rst_n);
+
+wire dly_pipe_flush_req = dly_flush_r;
+wire pipe_flush_req_real = pipe_flush_req | dly_pipe_flush_req;
+
+
+
+/////////////////////////////////////////////////////////////////////////
+//the IR reg to be used in EXU for decoding
+wire ir_valid_set;
+wire ir_valid_clr;
+wire ir_valid_ena;
+wire ir_valid_r;
+wire ir_valid_nxt;
+
+wire ir_pc_vld_set;
+wire ir_pc_vld_clr;
+wire ir_pc_vld_ena;
+wire ir_pc_vld_r;
+wire ir_pc_vld_nxt;
+
+//the ir valid is set when there is new inst fetched and no flush happening
+wire ifu_rsp_need_replay;
+wire pc_newpend_r;
+wire ifu_ir_i_ready;
+assign ir_valid_set = ifu_rsp_hsked & (~pipe_flush_req_real) & (~ifu_rsp_need_replay);
+assign ir_pc_vld_set = pc_newpend_r & ifu_ir_i_ready & (~pipe_flush_req_real) & (~ifu_rsp_need_replay);
+
+//the ir valid is cleared when it is accepted by EXU stage or the flush happending
+assign ir_valid_clr = ifu_ir_o_hsked | (pipe_flush_hsked & ir_valid_r);
+assign ir_pc_vld_clr = ir_valid_clr;
+
+assign ir_valid_ena = ir_valid_set | ir_valid_clr;
+assign ir_valid_nxt = ir_valid_set | (~ir_valid_clr);
+assign ir_pc_vld_ena = ir_pc_vld_set | ir_pc_vld_clr;
+assign ir_pc_vld_nxt = ir_pc_vld_set | (~ir_pc_vld_clr);
+
+sirv_gnrl_dfflr #(1) ir_valid_dfflr (ir_valid_ena,ir_valid_nxt,ir_valid_r,clk,rst_n);
+sirv_gnrl_dfflr #(1) ir_pc_vld_dfflr (ir_pc_vld_ena,ir_pc_vld_nxt,ir_pc_vld_r,clk,rst_n);
+
+
+
 
 
 
