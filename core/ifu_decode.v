fb0d77dddaea695abd266c4f0654aadbef9e57ff
heiallen <wangchongyang96@163.com>
2021-08-04 22:59:20 +0800

    delete ifu_decode.v

--- core/ifu_decode.v
+++
@@ -1,7 +0,0 @@
-
-
-wire [31:0] rv32_imm = ({32{rv32_imm_sel_i}} & rv32_i_imm)
-                    |   ({32{rv32_imm_sel_s}} & rv32_s_imm)
-                    |   ({32{rv32_imm_sel_b}} & rv32_b_imm)
-                    |   ({32{rv32_imm_sel_u}} & rv32_u_imm)
-                    |   ({32{rv32_imm_sel_j}} & rv32_j_imm);
